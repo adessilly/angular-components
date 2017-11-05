2b9f70a49f6b9d2feab577ea2ef00fd80f47c962
adessilly <adessilly@gmail.com>
2017-11-05 20:13:20 +0100

    update for angular5

--- src/app/module/angular2-components.module.ts
+++ src/app/module/angular2-components.module.ts
@@ -11,29 +11,38 @@ import { LoadingComponent }     from '../loadable/loading.component';
 import { LoadableDirective }    from '../loadable/loadable.directive';
 import { RadioComponent }       from '../radio/radio.component';
 import { DateComponent } from '../date/date.component';
-import { DatePickerComponent }  from "../date/datePicker.component";
+import { DatePickerComponent }  from '../date/datePicker.component';
 import { TabComponent }         from '../tab/tab.component';
 import { TextareaComponent }    from '../textarea/textarea.component';
-import { CheckboxComponent }    from "../check/checkbox.component";
-import { NumericComponent }     from "../numeric/numeric.component";
-import {ShowResumePipe} from "../pipes/show-resume.pipe";
-import {TextfieldComponent} from "../textfield/textfield.component";
-import {SelectComponent} from "../select/select.component";
-import {BoxComponent} from "../box/box.component";
-import {SharedService} from "../switchView/shared.service";
-import {ModalComponent} from "../switchView/modal.component";
-import {BeanFormulaireComponent} from "../design/bandeaux/beanFormulaire.component";
-import {SwitchViewComponent} from "../switchView/switchView.component";
-import {DateRangeComponent} from "../dateRange/dateRange.component";
-import {DateRangeInputComponent} from "../dateRange/dateRangeInput.component";
+import { CheckboxComponent }    from '../check/checkbox.component';
+import { NumericComponent }     from '../numeric/numeric.component';
+import {ShowResumePipe} from '../pipes/show-resume.pipe';
+import {TextfieldComponent} from '../textfield/textfield.component';
+import {SelectComponent} from '../select/select.component';
+import {BoxComponent} from '../box/box.component';
+import {SharedService} from '../switchView/shared.service';
+import {ModalComponent} from '../switchView/modal.component';
+import {BeanFormulaireComponent} from '../design/bandeaux/beanFormulaire.component';
+import {SwitchViewComponent} from '../switchView/switchView.component';
+import {DateRangeComponent} from '../dateRange/dateRange.component';
+import {DateRangeInputComponent} from '../dateRange/dateRangeInput.component';
 
 
 @NgModule({
   imports: [
       CommonModule, FormsModule, ReactiveFormsModule
   ],
-  declarations: [ /* SwitchViewComponent, SelectComponent, TextfieldComponent, DateRangeComponent, DateRangeInputComponent, DatePickerComponent, LoadingComponent, LoadableDirective, RadioComponent, DateComponent, TabComponent, TextareaComponent, CheckboxComponent, NumericComponent, ShowResumePipe, BoxComponent, ModalComponent, BeanFormulaireComponent */ ],
-  exports: [ /* SwitchViewComponent, SelectComponent, TextfieldComponent, DateRangeComponent, DateRangeInputComponent, DatePickerComponent, LoadingComponent, LoadableDirective, RadioComponent, DateComponent, TabComponent, TextareaComponent, CheckboxComponent, NumericComponent, ShowResumePipe, BoxComponent, ModalComponent, BeanFormulaireComponent */],
+  declarations: [ SwitchViewComponent, SelectComponent, TextfieldComponent, DateRangeComponent, DateRangeInputComponent,
+      DatePickerComponent,
+      LoadingComponent, LoadableDirective, RadioComponent, DateComponent, TabComponent, TextareaComponent, CheckboxComponent,
+      NumericComponent,
+      ShowResumePipe, BoxComponent, ModalComponent, BeanFormulaireComponent],
+  exports: [ SwitchViewComponent, SelectComponent, TextfieldComponent, DateRangeComponent, DateRangeInputComponent, DatePickerComponent,
+      LoadingComponent,
+      LoadableDirective, RadioComponent, DateComponent,
+      TabComponent, TextareaComponent, CheckboxComponent, NumericComponent, ShowResumePipe,
+      BoxComponent,
+      ModalComponent, BeanFormulaireComponent],
   providers: [SharedService],
   entryComponents: [BeanFormulaireComponent]
 })
